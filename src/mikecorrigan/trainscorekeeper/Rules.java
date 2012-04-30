4162160c7cfe0798aaf51200a606875b11f33b58
Mike Corrigan <corrigan@gmail.com>
2012-04-29 21:37:32 -0500

    Added 9-car train for 27 points.
    Signed-off-by: Mike Corrigan <corrigan@gmail.com>

--- src/mikecorrigan/trainscorekeeper/Rules.java
+++ src/mikecorrigan/trainscorekeeper/Rules.java
@@ -14,7 +14,7 @@
 package mikecorrigan.trainscorekeeper;
 
 public interface Rules {
-	final static int[] trainPoints = { 1, 2, 4, 7, 10, 15, -1, 21 };
+	final static int[] trainPoints = { 1, 2, 4, 7, 10, 15, -1, 21, 27 };
 	final static int[] trainContracts = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
 	final static int numTrainStations = 3;
 	final static int trainStationValue = 4;
